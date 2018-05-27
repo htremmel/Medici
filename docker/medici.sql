9e08a63c57c29330b7b78b37b52587bd5f1b794c
Hans@arch <hans.tremmel@gmail.com>
2018-05-27 10:40:55 -0700

    Learning tests with Docker and Dockerfile. -  Getting most things to build and run. -  Still don't have a configuration that will meet my usecase. *Current Usecase* 1.  Can access via ssh, phpmyadmin, and serve dist directory from web project. 2.  Has a php server as the api for the database. 3.  Will refresh and reserve website on changes from src folder.

--- docker/medici.sql
+++ docker/medici.sql
@@ -1,7 +1,6 @@
-DROP DATABASE medici;
 CREATE DATABASE medici;
 USE medici;
 CREATE TABLE transactions (
-    id int unsigned not null auto_increment PRIMARY KEY, 
+    id int unsigned not null auto_increment PRIMARY KEY,
     amount double signed not null
 );
--- docker/runTestEnv.sh
+++ docker/runTestEnv.sh
@@ -1,5 +1,5 @@
-docker run -d -p 49160:22 -p 49161:80 -p 49162:3306 -v /mnt/data/CloudStation/Projects/FinFam/website/docker/:/mnt/ wnameless/mysql-phpmyadmin
+docker run -d -p 8080:80 -p 2222:22 -v /mnt/data/CloudStation/Projects/FinFam/website/:/var/www/html sunglasses/lamp-ssh
 
-ssh root@localhost -p 49160
-open http://localhost:49161/phpmyadmin/
+ssh root@localhost -p 2222
+open http://localhost:8080/phpmyadmin/
 
