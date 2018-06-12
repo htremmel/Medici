9cde68145c57ccd9764b66e0e12af056df83dcdb
Hans@arch <hans.tremmel@gmail.com>
2018-06-12 12:07:38 -0700

    Cleaning before relooking at this problem.

--- sync.sh
+++
@@ -1,2 +0,0 @@
-rsync -avzru --exclude 'node_modules*' --exclude 'bower_components*' ./ /mnt/data/CloudStation/Projects/FinFam/website
-rsync -avzru --exclude 'node_modules*' --exclude 'bower_components*' /mnt/data/CloudStation/Projects/FinFam/website/ ./
